078ca2b174d6809fa32d48a6e82128d3958bedc0
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2023-01-21 14:13:44 +0330

    add json.server to gh-pages

--- src/services/CRUDContactService.js
+++ src/services/CRUDContactService.js
@@ -1,21 +1,26 @@
 import http from "./httpService";
 
 export function getContacts() {
-  return http.get("/contacts");
+  // return http.get("/contacts");
+  return http.get("");
 }
 
 export function getContact(id) {
-  return http.get(`/contacts/${id}`);
+  // return http.get(`/contacts/${id}`);
+  return http.get(`/${id}`);
 }
 
 export function postContact(bodyRequest) {
-  return http.post("/contacts", bodyRequest);
+  // return http.post("/contacts", bodyRequest);
+  return http.post("", bodyRequest);
 }
 
 export function putContact(id, bodyRequest) {
-  return http.put(`/contacts/${id}`, bodyRequest);
+  // return http.put(`/contacts/${id}`, bodyRequest);
+  return http.put(`/${id}`, bodyRequest);
 }
 
 export function deleteContact(id) {
-  return http.delete(`/contacts/${id}`);
+  // return http.delete(`/contacts/${id}`);
+  return http.delete(`/${id}`);
 }
